564dc6c22bd26c4dd797cd490b971d4a8d064a53
Ed <gardame@gmail.com>
2017-12-15 21:17:46 +1100

    added normalize.css, postcss and css import

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
-postcss = require('gulp-postcss');
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 
 
   gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-      .pipe()
+      .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
       .pipe(gulp.dest('./app/temp/styles'));
   });
 
